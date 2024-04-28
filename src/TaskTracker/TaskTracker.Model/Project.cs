82709f319f81b050ec12a00b5d8072de726b8c94
Сергей Пчельник <Sergeypchelnik@mail.ru>
2024-04-28 18:14:25 +0700

    feature(Project, ProjectManager, TaskStatus): создан класс Project для упарвления задачами, ProjectManager для сохранения задач в файле формата .json, а также перечисление TaskStatus для отслеживания статуса задачи

---
+++ src/TaskTracker/TaskTracker.Model/Project.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Model
+{
+    public class Project
+    {
+        public List<Task> Tasks { get; set; } = new();
+
+        /// <summary>
+        /// Поиск контакта по подстроке одного из полей.
+        /// </summary>
+        /// <param name="query">Записываемая пользователем строка</param>
+        /// <returns></returns>
+        public List<Task> FindContacts(string query)
+        {
+            if (query == null)
+            {
+                return Tasks;
+            }
+            var options = StringComparison.OrdinalIgnoreCase;
+            return Tasks.Where(c =>
+                c.Name.Contains(query, options)).ToList();
+        }
+    }
+}
---
+++ src/TaskTracker/TaskTracker.Model/ProjectManager.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+
+namespace TaskTracker.Model
+{
+    internal class ProjectManager
+    {
+        private static string _appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private static string _path = $"{_appDataPath}\\TaskTracker";
+        private static string _fileName = "\\TaskTracker.notes";
+        public string Path
+        {
+            set
+            {
+                _path = value;
+            }
+            get
+            {
+                return _path;
+            }
+        }
+        /// <summary>
+        /// Сохраняет проект по пути _folderPath.
+        /// </summary>
+        /// <param name="project"></param>
+        public void SaveProject(Project project)
+        {
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+            if (!File.Exists(_path + _fileName))
+            {
+                var fileStream = File.Create(_path + _fileName);
+                fileStream.Close();
+            }
+            File.WriteAllText(_path + _fileName, JsonConvert.SerializeObject(project));
+        }
+
+        /// <summary>
+        /// Загружает данные из пути _folderPath.
+        /// </summary>
+        /// <returns></returns>
+        public Project LoadProject()
+        {
+            try
+            {
+                Project project;
+                if (!File.Exists(_path + _fileName))
+                {
+                    return new Project();
+                }
+                using (StreamReader reader = new StreamReader(_path + _fileName))
+                {
+                    string jsonData = reader.ReadToEnd();
+                    project = JsonConvert.DeserializeObject<Project>(jsonData);
+                }
+                return project ?? new Project();
+            }
+            catch (Exception e)
+            {
+                return new Project();
+            }
+
+        }
+    }
+}
